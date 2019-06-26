be4d49344d79430c6f870375403f3a26fa74c6a0
HJS <HJS@DESKTOP-OPRDUOT>
2019-06-26 12:20:41 +0900

    점검완료!

--- mjmjmj/src/mjmjmj1/mjtest.java
+++ mjmjmj/src/mjmjmj1/mjtest.java
@@ -7,6 +7,7 @@ public class mjtest {
 		System.out.println("몇 번 째 날라 가냐");
 		System.out.println("11111111111111111111111111111");
 		System.out.println("2222222222222222222222222222222");
+		System.out.println("점검 완료!");
 	}
 
 }
