129ca586fe34af6d6a4f89ed63cafe2a05d6b296
HJS <HJS@DESKTOP-OPRDUOT>
2019-06-26 12:10:51 +0900

    2222222222222222222222222222222

--- mjmjmj/src/mjmjmj1/mjtest.java
+++ mjmjmj/src/mjmjmj1/mjtest.java
@@ -5,6 +5,7 @@ public class mjtest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("몇 번 째 날라 가냐");
+		System.out.println("11111111111111111111111111111");
 	}
 
 }
