e7f91350f48d174f8a751df4150cd2cda3acea61
HJS <HJS@DESKTOP-OPRDUOT>
2019-06-28 18:15:58 +0900

    2019.06.28 last commit ex00

--- ex00/src/test/java/org/zerock/persistence/TimeMapperTests.java
+++ ex00/src/test/java/org/zerock/persistence/TimeMapperTests.java
@@ -12,18 +12,23 @@ import lombok.extern.log4j.Log4j;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
-//ContextConfiguration(Class = { org.zerock.config.RootConfig.class } )
+//@ContextConfiguration(classes = { org.zerock.config.RootConfig.class } )
 @Log4j
 public class TimeMapperTests {
 
-	@Setter(onMethod_ = { @Autowierd })
-	private TimeMapper timeMapper;
-	
+	@Setter(onMethod_=@Autowired)
+	private TimeMapper timeMapper;	
 	
 	@Test
 	public void timeGetTime() {
 		log.info(timeMapper.getClass().getName());
 		log.info(timeMapper.getTime());
+	}
+	
+	@Test
+	public void testGetTime2() {
+		log.info("getTime2");
+		log.info(timeMapper.getTime2());
 	}
 	
 }
